3de9c98b9856768b00dc5f6962962abb21f84518
brunospbnu <brunosp423@gmail>
2026-06-08 08:50:52 -0300

    classes base

--- Jogo.java
+++ Jogo.java
@@ -1,3 +1,13 @@
 public class Jogo {
     
+    public Jogo() {
+        String[] tesouros = new String[15];
+        //.....
+        iniciar();
+    }
+
+    public void iniciar() {
+        //....
+        System.out.println("Iniciou o jogo");
+    }
 }
--- Principal.java
+++ Principal.java
@@ -1,4 +1,12 @@
 public class Principal {
-
-    
+   
+    public Principal() {
+        //código...
+        Jogo jogo = new Jogo();
+        //jogo.iniciar();
+    }
+ 
+    public static void main(String[] args) throws Exception {
+       new Principal();
+    }
 }
