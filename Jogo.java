c9e5b73f7440a0021137fd72d3e36077a0cd2f55
brunospbnu <brunosp423@gmail>
2026-06-08 08:57:59 -0300

    complementos da professora

--- Jogo.java
+++ Jogo.java
@@ -6,6 +6,17 @@ public class Jogo {
         iniciar();
     }
 
+    private String sortear() {
+        String[] tipos = {"VAZIO", "OURO", "DIAMANTE","RUBI", "BURADO", "COBRA", "ESPINHOS"};
+        /*tipos[0]= "VAZIO";
+        tipos[1] = "OURO";
+        tipos[2] = "DIAMANTE";*/
+
+        int posicao = (int)(Math.random() * 7);
+        //System.out.println(posicao + " " + tipos[posicao]);
+        return tipos[posicao];
+    }
+    
     public void iniciar() {
         //....
         System.out.println("Iniciou o jogo");
