7db46ddcee9d3de5b80d7e9bc43783bbb22ff919
brunospbnu <brunosp423@gmail>
2026-06-10 08:41:16 -0300

    corriji para evitar repetição da mensagem inicial

--- Principal.java
+++ Principal.java
@@ -2,7 +2,6 @@ public class Principal {
    
     public Principal() {
         Jogo jogo = new Jogo();
-        jogo.iniciar();
     }
  
     public static void main(String[] args) throws Exception {
